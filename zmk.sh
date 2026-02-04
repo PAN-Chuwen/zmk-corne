ff735213694042ef53a6294d2d881297e2785543
PAN-Chuwen <panchuwen2000@gmail.com>
2026-02-04 16:22:40 +0800

    fix: restore tmux handling (bash doesn't use zsh wrapper)

--- zmk.sh
+++ zmk.sh
@@ -327,7 +327,12 @@ cmd_draw() {
         # Display in terminal with chafa
         if command -v chafa &> /dev/null; then
             echo ""
-            chafa --size=120 "$png_file"
+            # Use kitty protocol with passthrough in tmux (Ghostty/Kitty)
+            if [ -n "$TMUX" ]; then
+                chafa --size=120 --format=kitty --passthrough=tmux "$png_file"
+            else
+                chafa --size=120 "$png_file"
+            fi
         else
             print_info "Install chafa for terminal preview: brew install chafa"
             open "$png_file"
@@ -335,7 +340,11 @@ cmd_draw() {
     else
         print_info "Install librsvg for PNG: brew install librsvg"
         if command -v chafa &> /dev/null; then
-            chafa --size=120 "$svg_file"
+            if [ -n "$TMUX" ]; then
+                chafa --size=120 --format=kitty --passthrough=tmux "$svg_file"
+            else
+                chafa --size=120 "$svg_file"
+            fi
         else
             open "$svg_file"
         fi
